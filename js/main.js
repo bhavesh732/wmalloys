fce140818192a9a9f78a12b8af45f1d3068b454e
bhavesh732 <bhaveshkhivesra@gmail.com>
2023-05-18 02:01:04 +0530

    emailer

--- js/main.js
+++ js/main.js
@@ -28,13 +28,31 @@ function hidefunction() {
   prevScrollpos = currentScrollPos;
 }
 
-// document.addEventListener("scroll", () => {
-//   if (
-//     document.body.scrollTop > 100 ||
-//     document.documentElement.scrollTop > 100
-//   ) {
-//     nav.className = " navscroll ";
-//   } else {
-//     nav.className = "  ";
-//   }
-// });
+function sendEmail() {
+  var name = $("[name='name']").val();
+  var company = $("[name='company']").val();
+  var designation = $("[name='designation']").val();
+  var email = $("[name='email']").val();
+  var phone = $("[name='phone']").val();
+  var subject = $("[name='subject']").val();
+  var body = $("[name='body']").val();
+
+  Email.send({
+    // Host: "smtp.gmail.com",
+    // Username: "mansunny282@gmail.com",
+    // Password: "billionaire19",
+    // To: "bhaveshkhivesra@gmail.com",
+    // From: "mansunny282@gmail.com",
+    Host: "smtp.elasticemail.com",
+    Username: "mansunny@smtp.com",
+    Password: "C064D4D952A30FBB4DA5B340AA951A50DD45",
+    To: "bhaveshkhivesra@gmail.com, sidd@wmalloys.com",
+    From: "mansunny282@gmail.com",
+    Subject: subject,
+    Body: `${body}, Name: ${name},\nCompany: ${company}, \nDesignation: ${designation}, \nEmail: ${email}, \nPhone: ${phone},`,
+  }).then(() =>
+    alert(
+      "Thank you for your interest, one of our experts will contact you soon."
+    )
+  );
+}
